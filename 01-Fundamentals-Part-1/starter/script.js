db9b8ef3c3f0f77970c8742090522820acf6546f
Patrick Höfling <patrickhoefling@gmail.com>
2024-11-12 15:06:20 +0100

    Finished Part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -53,11 +53,71 @@ console.log(typeof javascriptIsFun);
 
 // console.log();
 
-const age = 17;
+// const age = 17;
 
-if (age >= 18) {
-  console.log("Sarah can start driving license 🚗");
-} else {
-  const yearsLeft = 18 - age;
-  console.log(`${yearsLeft} until Sarah can start her driving license.`);
-}
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`${yearsLeft} until Sarah can start her driving license.`);
+// }
+
+// const inputYear = "1991";
+// console.log(Number(inputYear) + 18);
+
+// //type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
+
+// // 5 falsy values values: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(null));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean({}));
+
+// const money = 0;
+// if (money) {
+//   console.log("We are rich.");
+// } else {
+//   console.log("You are poor.");
+// }
+
+// let height = 0;
+// if (height || height == 0) {
+//   console.log("Height is defined");
+// } else {
+//   console.log("height is not defined");
+// }
+
+// const age = 18;
+// if (age === 18) console.log("You just became an adult");
+
+// const favourite = Number(prompt("What's your favourite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite === 23) {
+//   console.log("Cool! 23 is an amazing number!");
+// }
+
+// const day = "monday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("Plan my course structure");
+//     break;
+//   case "tuesday":
+//     console.log("prepare theory videos");
+//     break;
+
+//   default:
+//     break;
+// }
+
+const age = 18;
+age >= 18
+  ? console.log("I like to drink wine")
+  : console.log("I like to drink water");
