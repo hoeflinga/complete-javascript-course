364e878e95bf288e1fcf08331775d898e499229e
Patrick Höfling <patrickhoefling@gmail.com>
2024-11-12 13:53:26 +0100

    Finished Fundementals until step 19

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,63 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+//#region Country information section
+let country = "Germany";
+let continent = "Europa";
+let populationInMillions = "90";
+//#region
+console.log(
+  country +
+    " is located on the continent " +
+    continent +
+    " and has a population of " +
+    populationInMillions +
+    " million"
+);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 3);
+console.log(typeof "true");
+
+javascriptIsFun = null;
+console.log(typeof javascriptIsFun);
+*/
+
+// var job = "Programmer";
+// job = "Teacher";
+
+// lastName = "Höfling";
+// console.log(lastName);
+
+// const currentYear = 2037;
+// const ageJonas = currentYear - 1991;
+// const ageSarah = currentYear - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Jonas";
+// const job = "Teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(jonas);
+
+// console.log();
+
+const age = 17;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`${yearsLeft} until Sarah can start her driving license.`);
+}
